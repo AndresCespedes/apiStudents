f68adfc544fd71788f6949acb92320de19c06e6c
AndresCespedes <felipecespedes1999@hotmail.com>
2024-09-17 10:27:45 -0500

    Added the scaffolding for the project

---
+++ src/main/java/ApiStudents/ApiStudentsApplication.java
@@ -0,0 +1,13 @@
+package ApiStudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiStudentsApplication.class, args);
+	}
+
+}
